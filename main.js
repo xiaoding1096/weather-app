180afb161b667752549993dcf00f84fa38b4d3a2
NguyenDinh <hoangnguyendt1096@gmail.com>
2024-11-07 19:54:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const search = document.querySelector(".search");
+const city = document.querySelector(".city");
+const time = document.querySelector(".time");
+const celsius = document.querySelector(".celsius");
+const conditions = document.querySelector(".short-desc");
+const weather = document.getElementById("weather");
+const visibility = document.querySelector(".visibility span");
+const wind = document.querySelector(".wind-speed");
+const humidity = document.querySelector(".humidity");
+const body = document.querySelector("body");
+const content = document.querySelector(".content");
+async function changeWeatherUI() {
+  let capitalSearch = search.value.trim();
+  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${capitalSearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
+  let data = await fetch(apiURL).then((res) => res.json());
+  console.log(data);
+
+  content.classList.remove(".hide");
+  city.innerText = data.resolvedAddress;
+  let temp = Math.round((data.currentConditions.temp - 32) / 1.8);
+  celsius.innerText = temp;
+  conditions.innerText = data.currentConditions.conditions;
+  visibility.innerText = data.currentConditions.visibility + " Km";
+  wind.innerText = data.currentConditions.windspeed + " m/s";
+  humidity.innerText = data.currentConditions.humidity + " %";
+  time.innerText = new Date().toLocaleString(data.address);
+
+  body.setAttribute("class", "warm");
+  if (temp <= 15) {
+    body.setAttribute("class", "cold");
+  }
+}
+search.addEventListener("keypress", function (e) {
+  if (e.code === "Enter") {
+    changeWeatherUI();
+  }
+});
